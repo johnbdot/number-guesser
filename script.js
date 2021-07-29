ae68bd30f6cdec4897b161ad91a0f74f8a720469
John Bouakhasith <johnbeedot@gmail.com>
2021-07-29 15:18:09 -0400

    -

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget() {
+  return Math.floor(Math.random() * 10);
+}
+
+function compareGuesses(humanGuess, compGuess, secretTarget) {
+  if (humanGuess === secretTarget) {
+    return true;
+  } else if (compGuess === secretTarget) {
+    return false;
+  }
+}
+
+function updateScore(winner) {
+  if (winner === "human") {
+    humanScore += 1;
+  }
+  computerScore += 1;
+}
+
+function advanceRound() {
+  currentRoundNumber += 1;
+}
